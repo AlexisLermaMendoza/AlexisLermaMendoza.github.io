da4fdf24c8b504fff53f7d4c373a70568d0a4b36
AlexisLermaMendoza <156731149+AlexisLermaMendoza@users.noreply.github.com>
2024-01-26 19:26:16 -0600

    Create login.js

---
+++ login.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Hello, world!");
+    var inputEmail = this.documentElementById('email');
+    var inputPassword = this.documentElementById('password');
+    console.log(inputEmail);
+    console.log(inputPassword);
+});
